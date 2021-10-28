448041b7f29b1f62927a5e4836521998e4f82840
rileybusche <riley.busche@gmail.com>
2021-10-28 07:38:57 -0500

    Adding new files

--- gui/src/components/FileSearch.js
+++ gui/src/components/FileSearch.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
 
+import '../style/FileSearch.css';
+
 function FileSearch() {
+
+    const [filePath, setFilePath] = useState('Folder Location...');
+
+    function handleChange(e) {
+        console.log(e.target.files[0].path);
+        setFilePath(e.target.files[0].path);
+    }
+
     return(
         <div className='File-Search-Container'>
 
             {/* <input className='Search-Bar' type="file" id="file-upload" name="name" /> */}
-            <h2 className='Search-Bar'>File Location Place Holder</h2>
+            <h3 className='Search-Bar'>{filePath}</h3>
             <div className='File-Search-Button-Container'>
                 <label for='files' className='Search-Button'>Select Folder</label>
-                <input id='files' type='file' className='File-Search-Button'/>
+                <input id='files' type='file' className='File-Search-Button' onChange={handleChange}/>
             </div>
         
         </div>
